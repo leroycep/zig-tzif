a02ecc5426e0fe8292a3d043a91c3356542a7953
LeRoyce Pearson <leroycepearson@geemili.xyz>
2021-02-12 00:14:12 -0700

    Rename from tzfile to tzif; add dump example

--- build.zig
+++ build.zig
@@ -2,13 +2,24 @@ const Builder = @import("std").build.Builder;
 
 pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
-    const lib = b.addStaticLibrary("tzfile", "tzfile.zig");
+    const lib = b.addStaticLibrary("tzif", "tzif.zig");
     lib.setBuildMode(mode);
     lib.install();
 
-    var main_tests = b.addTest("tzfile.zig");
+    var main_tests = b.addTest("tzif.zig");
     main_tests.setBuildMode(mode);
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&main_tests.step);
+
+    const target = b.standardTargetOptions(.{});
+    const dump_exe = b.addExecutable("dump", "examples/dump.zig");
+    dump_exe.addPackagePath("tzif", "tzif.zig");
+    dump_exe.setBuildMode(mode);
+    dump_exe.setTarget(target);
+
+    const run_dump_example = dump_exe.run();
+    
+    const run_dump_example_step = b.step("example-dump", "Run the `dump` example");
+    run_dump_example_step.dependOn(&run_dump_example.step);
 }
---
+++ examples/dump.zig
@@ -0,0 +1,18 @@
+const std = @import("std");
+const tzif = @import("tzif");
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = &gpa.allocator;
+
+    const cwd = std.fs.cwd();
+
+    const localtime_file = try cwd.openFile("/etc/localtime", .{});
+    const localtime = try tzif.parse(allocator, localtime_file.reader(), localtime_file.seekableStream());
+    defer localtime.deinit();
+
+    std.log.info("TZ string: {s}", .{localtime.string});
+    std.log.info("TZif version: {s}", .{localtime.version.string()});
+    std.log.info("{} transition times", .{localtime.transitionTimes.len});
+}
